a3f4563daaf9b97a60d55ab58e29f4f6a26d7f91
zubia-d <dahrizubia222@gmail.com>
2025-08-06 21:22:22 +0500

    abx

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-project/",
-
+  base: "/World-Atlas---React-Project/",
 })
